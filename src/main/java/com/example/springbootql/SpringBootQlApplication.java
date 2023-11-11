ff943991ba8658933e0209f4b98050af9f4750bd
Nurudeen Abdulmalik <abdulmaliknurudeen4@gmail.com>
2023-11-11 17:42:20 +0100

    first commit

---
+++ src/main/java/com/example/springbootql/SpringBootQlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootql/SpringBootQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
